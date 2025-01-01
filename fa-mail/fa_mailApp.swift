3fe2c8daa30e73be4b758305f89eb36f0001eed2
sebash <sebcampos@criptext.com>
2024-12-31 17:39:26 -0800

    Initial Commit

---
+++ fa-mail/fa_mailApp.swift
@@ -0,0 +1,17 @@
+//
+//  fa_mailApp.swift
+//  fa-mail
+//
+//  Created by Sebastian Campos on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct fa_mailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
